748555d28685a8eee6b6d14815eeaefc428c4e83
al <usernamehw@gmail.com>
2020-04-14 10:23:11 +0300

    Improve types

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,39 @@
 export interface IConfig {
+	/**
+	 * A bit complicated...
+	 */
 	commands: {
-		[key: string]: string | string[] | IRegister & IFolder;
+		[title: string]: string | ICommandArray | IRegister & IFolder & ICommandObject;
 	};
 	collapseFoldersByDefault: boolean;
 }
-
+/**
+ * And array of either strings or ICommandsObject or a mix of them
+ */
+export type ICommandArray = (string | ICommandObject)[];
+/**
+ * Just an object. Very similar to keybindings.json object type.
+ */
+export interface ICommandObject {
+	command: string;
+	delayBefore: number;
+	args: any;
+}
+/**
+ * And object to register a sequence of commands under 1 id
+ */
 export interface IRegister {
 	registerId: 'string';
-	sequence: any[];
+	sequence: ICommandArray;
 	excludeFromView: boolean;
 }
+/**
+ * Nested/folders/hierarchy
+ */
 export interface IFolder {
-	isFolder: boolean;
 	items: IConfig['commands'];
 }
-
+// ──────────────────────────────────────────────────────────────────────
 export interface IToggleSetting {
 	setting: 'string';
 	value: string | any[];
